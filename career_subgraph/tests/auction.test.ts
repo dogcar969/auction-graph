d4ee1ea9c867497193838d35393280124ce1bf58
dogcar <2443162935@qq.com>
2024-06-05 13:51:06 +0800

    Initialize subgraph

--- career_subgraph/tests/auction.test.ts
+++ career_subgraph/tests/auction.test.ts
@@ -7,23 +7,24 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { Approval } from "../generated/schema"
-import { Approval as ApprovalEvent } from "../generated/AnimalNft/AnimalNft"
-import { handleApproval } from "../src/animal-nft"
-import { createApprovalEvent } from "./animal-nft-utils"
+import { ExampleEntity } from "../generated/schema"
+import { Bid } from "../generated/Auction/Auction"
+import { handleBid } from "../src/auction"
+import { createBidEvent } from "./auction-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
-    let approved = Address.fromString(
+    let nftAddress = Address.fromString(
       "0x0000000000000000000000000000000000000001"
     )
     let tokenId = BigInt.fromI32(234)
-    let newApprovalEvent = createApprovalEvent(owner, approved, tokenId)
-    handleApproval(newApprovalEvent)
+    let bider = Address.fromString("0x0000000000000000000000000000000000000001")
+    let price = BigInt.fromI32(234)
+    let newBidEvent = createBidEvent(nftAddress, tokenId, bider, price)
+    handleBid(newBidEvent)
   })
 
   afterAll(() => {
@@ -33,26 +34,32 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("Approval created and stored", () => {
-    assert.entityCount("Approval", 1)
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "owner",
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "nftAddress",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "approved",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "tokenId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "bider",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "price",
       "234"
     )
 
